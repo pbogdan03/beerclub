a6de8cce5a2ed496d166dc0cba87355a46dde4ae
pbogdan03 <p_bogdan03@yahoo.com>
2015-12-17 16:07:24 +0200

    added handlebars views

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Beerclub
 
+### [>>>>> LINK HERE <<<<<](https://quiet-fortress-1367.herokuapp.com/)
+
 ## Setup online environment
 
 ### Github
@@ -22,6 +24,15 @@
 4. Create 404 page
 5. Create 500 page
 6. Start the server using the port and log something to the console
+
+## Setup some views
+
+1. `npm install --save express-handlebars` or __EJS__ or __Jade__
+2. Require the new resource and set the app view engine to __Handlebars__ 
+3. Create *views/layouts/main.handlebars* and *views/{404, 500, home}.handlebars* views
+4. Refactor the server side routes to render the newly created views
+
+
 
 
 
--- server.js
+++ server.js
@@ -2,21 +2,30 @@ var express = require('express');
 
 var app = express();
 
+// set handlebars as the view engine
+var handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
-// 404 page 
-app.use(function(req, res) {
-	res.type('text/plain');
-	res.status(404);
-	res.send('404 - Not found');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home');
 });
 
-// 500 page
+// 404 catch-all (middleware)
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+// 500 error handler (middleware)
 app.use(function(err, req, res, next) {
 	console.log(err.stack);
-	res.type('text/plain');
 	res.status(500);
-	res.send('500 - Server Error');
+	res.render('500');
 });
 
 // start the server
