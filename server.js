c8ecbd6e7b1b74a50b242773162fc6e3caa85d56
pbogdan03 <p_bogdan03@yahoo.com>
2015-12-18 17:42:23 +0200

    connected mongolab

--- server.js
+++ server.js
@@ -1,4 +1,55 @@
 var express = require('express');
+var	mongoose = require('mongoose'); //dbuser: bogdanp, dbpass: clubbingb33r
+
+mongoose.connect('mongodb://bogdanp:clubbingb33r@ds033175.mongolab.com:33175/beerclubdb', function(err) {
+	if (err) throw err;
+	console.log('Connected to MongoLab...');
+});
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+	name: {
+		first: String,
+		last: String
+	},
+	username: String,
+	pass: String,
+	beers: [{type: Schema.Types.ObjectId, ref: 'Beer'}]
+});
+var beerSchema = new Schema({
+	title: String,
+	instagram: String,
+	description: String,
+	untappdRating: Number,
+	userRating: Number,
+	date: Date,
+	user: {type: Number, ref: 'User'}
+});
+
+var User = mongoose.model('User', userSchema);
+var Beer = mongoose.model('Beer', beerSchema);
+
+var bogdan = new User({
+	name: {
+		first: 'Bogdan',
+		last: 'P'
+	},
+	username: 'p_bogdan03',
+	pass: 'hello',
+	beers: []
+});
+
+User.findOne({'username': 'p_bogdan03'}, function(err, user) {
+	if (err) throw err;
+	if (!user) {
+		bogdan.save(function(err) {
+			if (err) throw err;
+			console.log('bogdan user saved...');
+		});
+	} else {
+		console.log('bogdan user already exists...');
+	}
+});
 
 var app = express();
 
