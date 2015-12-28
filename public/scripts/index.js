47f9a7e0e67adf792776f879055f6452ef27a003
pbogdan03 <p_bogdan03@yahoo.com>
2015-12-28 19:58:27 +0200

    started backbone.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,1 +1,32 @@
-console.log("Hello");
+// Backbone Model
+
+var User = Backbone.Model.extend({
+	defaults: {
+		name: '',
+		username: ''
+	}
+});
+
+var Beer = Backbone.Model.extend({
+	defaults: {
+		title: '',
+		description: ''
+	}
+});
+
+// Backbone Collection
+
+var Users = Backbone.Collection.extend({});
+var Beers = Backbone.Collection.extend({});
+
+// instantiate two models
+
+var user1 = new User({
+	name: 'Bogdan',
+	username: 'bogdan'
+});
+
+var beer1 = new Beer({
+	title: 'Morning Glory',
+	description: 'Best beer ever...'
+});
