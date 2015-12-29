08b3e77ed7831f12f11e569601aa20a229ca4475
pbogdan03 <p_bogdan03@yahoo.com>
2015-12-29 16:18:33 +0200

    remove user collection and added backbone views

--- README.md
+++ README.md
@@ -21,23 +21,21 @@
 1. Create *server.js*
 2. Import express module
 3. Set port 3000 or use from environment variables
-4. Create 404 page
-5. Create 500 page
-6. Start the server using the port and log something to the console
+4. Create error and index page
+5. Start the server using the port and log something to the console
 
 ## Setup some views
 
 1. `npm install --save express-handlebars` or __EJS__ or __Jade__
 2. Require the new resource and set the app view engine to __Handlebars__ 
-3. Create *views/layouts/main.handlebars* and *views/{404, 500, home}.handlebars* views
+3. Create *views/error.handlebars* and *views/index.handlebars* views
 4. Refactor the server side routes to render the newly created views
 
 ## Connect with MongoLab
 
 1. `npm install --save mongoose` and require it
 2. Connect to mongolab
-3. Create *User* and *Beer* schemas
-4. Create a user and save it to db if it does not exist
+3. Create *Beer* schema
 
 ## Setup environment variables
 
@@ -54,6 +52,15 @@
 2. Setup *nodemon* to watch for changes and reload the server on port 3000
 3. `npm install --save-dev tiny-lr` and setup the *livereload* server to send and watch changes on file change
 4. `npm install --save-dev connect-livereload` to inject the *livereload* script that listens to changes, on the necessary client html pages
+
+### Backbone
+
+## Initial setup
+
+1. Create *index.html* in the public folder to be served by **Express**
+2. Add script tags for the necessary dependecies in the page.
+3. Create *scripts/index.js*
+4. Create a *Beer* model and a *BeerView* as well as a *Beers* collection and *BeersView*
 
 
 
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,12 +1,5 @@
 // Backbone Model
 
-var User = Backbone.Model.extend({
-	defaults: {
-		name: '',
-		username: ''
-	}
-});
-
 var Beer = Backbone.Model.extend({
 	defaults: {
 		title: '',
@@ -16,17 +9,62 @@ var Beer = Backbone.Model.extend({
 
 // Backbone Collection
 
-var Users = Backbone.Collection.extend({});
 var Beers = Backbone.Collection.extend({});
 
 // instantiate two models
 
-var user1 = new User({
-	name: 'Bogdan',
-	username: 'bogdan'
+// var beer1 = new Beer({
+// 	title: 'Morning Glory',
+// 	description: 'Best beer ever...'
+// });
+// var beer2 = new Beer({
+// 	title: 'Staropramen',
+// 	description: 'Second best beer ever...'
+// });
+
+// instantiate a collection
+
+var beers = new Beers();
+
+// Backbone Views
+
+var BeerView = Backbone.View.extend({
+	model: new Beer(),
+	initialize: function() {
+		this.template = _.template($('.beers-list-template').html());
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+
+});
+var BeersView = Backbone.View.extend({
+	model: beers,
+	el: $('.beers-list'),
+	initialize: function() {
+		this.model.on('add', this.render, this);
+	},
+	render: function() {
+		var self = this;
+		this.$el.html('');
+		_.each(this.model.toArray(), function(beer) {
+			self.$el.append((new BeerView({model: beer})).render().$el);
+		});
+		return this;
+	}
 });
 
-var beer1 = new Beer({
-	title: 'Morning Glory',
-	description: 'Best beer ever...'
+var beersView = new BeersView();
+
+$(document).ready(function() {
+	$('.add-beer').on('click', function() {
+		var beer = new Beer({
+			title: $('.title-input').val(),
+			description: $('.description-input').val()
+		});
+		$('.title-input').val('');
+		$('.description-input').val('');
+		beers.add(beer);
+	});
 });
